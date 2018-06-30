4da8755d86a1c692e14cdab397d5b5740c8e0b8d
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2018-06-30 17:07:08 +0300

    undefined params

--- example.js
+++ example.js
@@ -1,35 +1,29 @@
 const IPC = require('./index');
 
-let myIPC = IPC({
-  debug: true
-});
+let myIPC = IPC();
 
 myIPC.on('connect', _ => {
   console.log('connect event');
   myIPC
     .subscribe('0x00/playback/state')
     .on('subscribe', _ => {
-      myIPC
-        .publish('0x00/playback/state', "Hello, friend");
+      myIPC.publish('0x00/playback/state', 'Hello, friend');
     })
     .on('message', message => {
       console.log(`got message: ${message}`);
-      myIPC
-        .unsubscribe('0x00/playback/state');
+      myIPC.unsubscribe('0x00/playback/state');
 
       myIPC
         .subscribe('hello')
         .on('subscribe', _ => {
-          myIPC
-            .publish('hello', 'friend');
+          myIPC.publish('hello', 'friend');
         })
         .on('message', message => {
           console.log(`another message: ${message}`);
-        })
+        });
     });
 });
 
-
 myIPC.on('error', err => {
-  console.log(`err ${err}`)
+  console.log(`err ${err}`);
 });
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const EE = require('events');
 const redis = require('redis');
 
-const _IPC = (params) => {
+const _IPC = params => {
   let self = new EE();
+  let _params = Object.assign({}, params);
   // debugging
   self._debug = false;
-  if (Object.prototype.hasOwnProperty.call(params, 'debug')) {
-    self._debug = params.debug;
+  if (Object.prototype.hasOwnProperty.call(_params, 'debug')) {
+    self._debug = _params.debug;
   }
   self.debug = (...args) => {
     if (self._debug) {
@@ -16,8 +17,8 @@ const _IPC = (params) => {
 
   // redis client pub/sub
   self.path = '/var/run/redis/redis.sock';
-  if (Object.prototype.hasOwnProperty.call(params, 'path')) {
-    self.path = params.path;
+  if (Object.prototype.hasOwnProperty.call(_params, 'path')) {
+    self.path = _params.path;
   }
   self._client = {};
   self._client.connectPublisher = _ => {
@@ -25,9 +26,9 @@ const _IPC = (params) => {
       self._client.pub = redis.createClient(self.path);
       self._client.pub.on('connect', _ => {
         self.debug(`Redis client.pub connected`);
-        resolve()
+        resolve();
       });
-      self._client.pub.on('error', (err) => {
+      self._client.pub.on('error', err => {
         self.debug(`Redis client.pub error ${err}`);
         reject(err);
       });
@@ -38,9 +39,9 @@ const _IPC = (params) => {
       self._client.sub = redis.createClient(self.path);
       self._client.sub.on('connect', _ => {
         self.debug(`Redis client.sub connected`);
-        resolve()
+        resolve();
       });
-      self._client.sub.on('error', (err) => {
+      self._client.sub.on('error', err => {
         self.debug(`Redis client.sub error ${err}`);
         reject(err);
       });
@@ -48,20 +49,21 @@ const _IPC = (params) => {
   };
 
   // now connect both
-  self._client.connectPublisher()
+  self._client
+    .connectPublisher()
     .then(_ => {
-      return self._client.connectSubscriber()
+      return self._client.connectSubscriber();
     })
     .then(_ => {
       self.debug(`Redis client.pub/sub connected`);
       self.emit('connect');
     })
     .catch(err => {
-      self.debug(`Redis client.pub/sub error ${err}`)
+      self.debug(`Redis client.pub/sub error ${err}`);
     });
 
   // public funcs
-  self.subscribe = (channel) => {
+  self.subscribe = channel => {
     let _ee = new EE();
     self._client.sub.on('subscribe', (_channel, _count) => {
       if (_channel === channel) {
@@ -71,21 +73,25 @@ const _IPC = (params) => {
     });
     self._client.sub.on('message', (_channel, _message) => {
       if (_channel === channel) {
-        self.debug(`Redis client.sub got message: "${_message}" from channel "${_channel}"`);
+        self.debug(
+          `Redis client.sub got message: "${_message}" from channel "${_channel}"`
+        );
         _ee.emit('message', _message);
       }
     });
     self._client.sub.subscribe(channel);
+
     return _ee;
   };
 
-  self.unsubscribe = (channel) => {
+  self.unsubscribe = channel => {
     self._client.sub.unsubscribe(channel);
   };
 
   self.publish = (channel, message) => {
     self._client.pub.publish(channel, message);
   };
+
   return self;
 };
 
